31b45871e53fe66178a5fd9c62a9e540427c7b24
Vipul Prashar <prashar.vipul@gmail.com>
2026-05-07 01:40:02 -0500

    Hide navigation announcer visually

--- src/components/AccessibleNavigationAnnouncer.js
+++ src/components/AccessibleNavigationAnnouncer.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+const visuallyHiddenStyle = {
+  border: 0,
+  clip: 'rect(0 0 0 0)',
+  height: 1,
+  margin: -1,
+  overflow: 'hidden',
+  padding: 0,
+  position: 'absolute',
+  whiteSpace: 'nowrap',
+  width: 1
+};
+
 const AccessibleNavigationAnnouncer = () => {
   return (
-    <div className="sr-only" role="status" aria-live="polite" aria-atomic="true">
+    <div
+      style={visuallyHiddenStyle}
+      role="status"
+      aria-live="polite"
+      aria-atomic="true"
+    >
       Navigated to app/dashboard page.
     </div>
   )
